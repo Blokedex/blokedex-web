e3985e1453cf51da2ec4fffac8e9988b2924ef79
Sebilebi <80194326+Sebilebi@users.noreply.github.com>
2026-09-07 17:28:15 +0200

    Añade cuenta atrás y arranque sin pnpm

--- iniciar-pagina.bat
+++ iniciar-pagina.bat
@@ -1,7 +1,7 @@
 @echo off
 pushd "%~dp0"
 
-start "Blokedex server" cmd /k "pnpm start"
+start "Blokedex server" cmd /k "node server.js"
 ping 127.0.0.1 -n 3 >nul
 start "" "http://localhost:3000"
 
